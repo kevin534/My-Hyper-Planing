9b2d2a49c12cf34876ad8ee17ff7dea49f55254b
“Registe <regisdjessou2@gmail.com>
2021-12-06 14:58:34 +0100

    Update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.hyperplanning {
     requires java.sql;
     requires javafx.graphics;
     requires javafx.fxml;
+    requires javafx.controls;
 
     opens com.hyperplanning to javafx.fxml;
     exports com.hyperplanning;
